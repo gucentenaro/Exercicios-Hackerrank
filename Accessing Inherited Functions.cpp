4454d7d3e58f098b39c74f10cd3f20a02823a496
gucentenaro <166958399+gucentenaro@users.noreply.github.com>
2025-06-12 21:50:03 -0300

    Add files via upload

---
+++ Accessing Inherited Functions.cpp
@@ -0,0 +1,106 @@
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class A {
+protected:  //Acessivel por classes derivadas (heranca)
+    void func(int &val) {
+        val *= 2;   //multiplica o valor por 2
+        callA++;    //saber quantas vezes func da classe A foi chamada
+    }
+private:
+    static int callA;   //static pq depende da classe e nao do objeto
+public:
+    static int getA() {
+        return callA;   //retornar o valor para a main
+    }  
+};
+int A::callA = 0;   //variavel callA da classe A
+
+
+class B {
+protected:
+    void func(int &val) {
+        val *= 3;   //multiplicar o valor por 3
+        callB++;    //saber quantas vezes func da classe B foi chamada
+    }
+private:
+    static int callB;
+public:
+    static int getB() {
+        return callB;
+    }
+};
+int B::callB = 0;
+
+
+class C {
+protected:
+    void func(int &val) {
+        val *= 5;   //multiplicar o valor por 5
+        callC++;    //saber quantas vezes func da classe C foi chamada
+    }
+private:
+    static int callC;
+public:
+    static int getC() {
+        return callC;
+    }
+};
+int C::callC = 0;
+
+
+//classe D herda A, B e C
+class D : public A, public B, public C {
+    int val;
+
+
+public:
+    //construtor
+    D() {
+        val = 1;    //neutro na multiplicacao
+    }
+
+
+    // Atualiza val ate chegar em new_val, chamando as func de A, B e C
+    void update_val(int new_val) {
+        while (new_val % 2 == 0) {
+            A::func(val);
+            new_val /= 2;
+        }
+        while (new_val % 3 == 0) {
+            B::func(val);
+            new_val /= 3;
+        }
+        while (new_val % 5 == 0) {
+            C::func(val);
+            new_val /= 5;
+        }
+    }
+
+
+    //impressao dos resultados
+    void check(int new_val) {
+        update_val(new_val);
+        cout << "Value = " << val << endl;
+        cout << "A's func called " << getA() << " times" << endl;
+        cout << "B's func called " << getB() << " times" << endl;
+        cout << "C's func called " << getC() << " times" << endl;
+    }
+};
+
+
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    D d;    //cria um objeto d da classe D
+    int new_val;
+    cin >> new_val;
+    d.check(new_val);   //chama para as estatisticas finais
+    return 0;
+}
---
+++ Overload Operators.cpp
@@ -0,0 +1,57 @@
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Complex {
+public:
+    int a, b;
+
+
+    //como nao se soma objetos personalizados, usa esse operador e os dois tipos que vc quer q ele some, assim quando + eh usado ele chama essa funcao
+    Complex operator+(const Complex& other) {   //other eh outro num complexo q sera somado
+        Complex result;
+        result.a = a + other.a; //soma as partes reais
+        result.b = b + other.b; //soma as partes imaginarias
+        return result;
+    }
+};
+
+
+//imprime o num complexo no formato certo, ent quando usar cout imprimira dessa maneira
+ostream& operator<<(ostream& out, const Complex& c) {
+    out << c.a << "+i" << c.b;  //c eh o objeto
+    return out;
+}
+
+
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+   
+    Complex x, y, z;
+    string s1, s2;
+
+
+    //le os complexos como strings
+    cin >> s1 >> s2;
+
+
+    //converte a string para um char
+    sscanf(s1.c_str(), "%d+i%d", &x.a, &x.b);
+    sscanf(s2.c_str(), "%d+i%d", &y.a, &y.b);
+
+
+    // Soma
+    z = x + y;
+
+
+    // Resultado
+    cout << z << endl;
+   
+    return 0;
+}
